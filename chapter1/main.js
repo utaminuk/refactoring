efd851e88c7784a4b88e44a3c256200fa8abaf28
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-03 15:49:36 +0900

    [refact] amountForからplay変数を削除
    - play変数はplayFor関数にて取得する

--- chapter1/main.js
+++ chapter1/main.js
@@ -21,7 +21,7 @@ function statement(invoice, plays) {
   function amountFor(aPerformance, play) {
     let result = 0;
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case 'tragedy':
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -36,7 +36,7 @@ function statement(invoice, plays) {
         this.Amount += 300 + aPerformance.audience;
         break;
       default:
-        throw new Error(`unknown type: ${play.type}`);
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
     }
 
     return result;
