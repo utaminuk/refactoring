fbed81c7cd705ba848e338bc8f11e0aaa8e1e709
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-03 10:42:25 +0900

    [refact] 1公演の価格を処理するswitch文をamountFor関数に切り出し

--- chapter1/main.js
+++ chapter1/main.js
@@ -1,6 +1,30 @@
 const plays = require('./plays.json');
 const invoices = require('./invoices.json');
 
+function amountFor(perf, play) {
+  let thisAmount = 0;
+
+  switch (play.type) {
+    case 'tragedy':
+      thisAmount = 40000;
+      if (perf.audience > 30) {
+        thisAmount += 1000 * (perf.audience - 30);
+      }
+      break;
+    case 'comedy':
+      thisAmount = 30000;
+      if (perf.audience > 20) {
+        thisAmount += 1000 + 500 * (perf.audience - 20);
+      }
+      this.Amount += 300 + perf.audience;
+      break;
+    default:
+      throw new Error(`unknown type: ${play.type}`);
+  }
+
+  return thisAmount;
+}
+
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -14,25 +38,8 @@ function statement(invoice, plays) {
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
-    let thisAmount = 0;
+    let thisAmount = amountFor(perf, play);
 
-    switch (play.type) {
-      case 'tragedy':
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
-        }
-        break;
-      case 'comedy':
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 1000 + 500 * (perf.audience - 20);
-        }
-        this.Amount += 300 + perf.audience;
-        break;
-      default:
-        throw new Error(`unknown type: ${play.type}`);
-    }
     // ボリューム特典のポイントを換算
     volumeCredits += Math.max(perf.audience - 30, 0);
 
