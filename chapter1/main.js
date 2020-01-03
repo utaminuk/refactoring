a3de318bd1b2bd50be7e746f7a01aa13bd1aed3e
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-03 09:16:48 +0900

    [feat] statement関数を追加

--- chapter1/main.js
+++ chapter1/main.js
@@ -4,10 +4,51 @@ const invoices = require('./invoices.json');
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
-  let result = `Statement for ${invoice.cutomer}\n`;
+  let result = `${invoice.customer} の支払い\n`;
 
-  console.log(plays);
-  console.log(invoices);
+  const price = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minumFractionDigits: 2
+  });
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy':
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 1000 + 500 * (perf.audience - 20);
+        }
+        this.Amount += 300 + perf.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+    // ボリューム特典のポイントを換算
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // 喜劇のときは10人につき、さらにポイントを加算
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 注文の内訳を出力
+    result += ` ${play.name}: ${price.format(thisAmount / 100)} (${
+      perf.audience
+    }席) \n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `支払額は${price.format(totalAmount / 100)}\n`;
+  result += `次回使える特典は${volumeCredits}ポイント\n`;
+  return result;
 }
 
-statement(invoices, plays);
+console.log(statement(invoices[0], plays));
