17f6ec2de6b51b83a14e462cc0fe54f6cff9b963
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-03 10:36:22 +0900

    [test] テストコードを追加
    - テストライブラリではなく直接関数を実行してテストする

--- chapter1/main.js
+++ chapter1/main.js
@@ -51,4 +51,18 @@ function statement(invoice, plays) {
   return result;
 }
 
-console.log(statement(invoices[0], plays));
+// テストコード
+const expected = `BigCo の支払い
+ Hamlet: $650.00 (55席) 
+ As You Like It: $385.00 (35席) 
+ Othello: $500.00 (40席) 
+支払額は$1,535.00
+次回使える特典は47ポイント
+`;
+
+const status = expected == statement(invoices[0], plays);
+const message = status ? '✔ : OK' : '✖ : ERROR!!';
+console.log(message);
+
+if (!status) console.log(statement(invoices[0], plays));
+if (!status) console.log(expected);
