5d88c78f6ec8fd5be8ed6355aca74b5b2075bcd1
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-03 15:28:25 +0900

    [refact] amountForを関数内関数に変更

--- chapter1/main.js
+++ chapter1/main.js
@@ -1,30 +1,6 @@
 const plays = require('./plays.json');
 const invoices = require('./invoices.json');
 
-function amountFor(perf, play) {
-  let thisAmount = 0;
-
-  switch (play.type) {
-    case 'tragedy':
-      thisAmount = 40000;
-      if (perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case 'comedy':
-      thisAmount = 30000;
-      if (perf.audience > 20) {
-        thisAmount += 1000 + 500 * (perf.audience - 20);
-      }
-      this.Amount += 300 + perf.audience;
-      break;
-    default:
-      throw new Error(`unknown type: ${play.type}`);
-  }
-
-  return thisAmount;
-}
-
 function statement(invoice, plays) {
   let totalAmount = 0;
   let volumeCredits = 0;
@@ -35,6 +11,30 @@ function statement(invoice, plays) {
     currency: 'USD',
     minumFractionDigits: 2
   });
+
+  function amountFor(perf, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+      case 'tragedy':
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case 'comedy':
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 1000 + 500 * (perf.audience - 20);
+        }
+        this.Amount += 300 + perf.audience;
+        break;
+      default:
+        throw new Error(`unknown type: ${play.type}`);
+    }
+
+    return thisAmount;
+  }
 
   for (let perf of invoice.performances) {
     const play = plays[perf.playID];
