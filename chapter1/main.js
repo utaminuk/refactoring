ef17f12e86e4f404cd2b96f34a4fd80e418f6b8f
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-04 11:21:09 +0900

    [refact] 統計処理のforループをreduceに変更

--- chapter1/main.js
+++ chapter1/main.js
@@ -62,20 +62,12 @@ function statement(invoice, plays) {
 
   // 総課金額を計算
   function totalAmount(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.amount, 0);
   }
 
   // ボリューム特典の集計
   function totalVolumeCredits(data) {
-    let result = 0;
-    for (let perf of data.performances) {
-      result += perf.volumeCredits;
-    }
-    return result;
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
   }
 }
 
