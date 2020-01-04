f97fd0ff8277b62ef10d6980eb0666bcb80d6cad
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-04 12:30:51 +0900

    [feat] 集計結果のHTML出力を追加
    - htmlStatementをstatement.mjsに追加

--- chapter1/statement.mjs
+++ chapter1/statement.mjs
@@ -4,6 +4,10 @@ export function statement(invoice, plays) {
   return renderPlainText(createStatementData(invoice, plays));
 }
 
+export function htmlStatement(invoice, plays) {
+  return renderHtml(createStatementData(invoice, plays));
+}
+
 // 価格表示フォーマット関数
 function usd(aNumber) {
   return new Intl.NumberFormat('en-US', {
@@ -13,7 +17,8 @@ function usd(aNumber) {
   }).format(aNumber / 100);
 }
 
-function renderPlainText(data, plays) {
+// プレーンテキストフォーマット出力
+function renderPlainText(data) {
   let result = `${data.customer} の支払い\n`;
 
   for (let perf of data.performances) {
@@ -23,5 +28,25 @@ function renderPlainText(data, plays) {
 
   result += `支払額は${usd(data.totalAmount)}\n`;
   result += `次回使える特典は${data.totalVolumeCredits}ポイント\n`;
+  return result;
+}
+
+// HTMLフォーマット出力
+function renderHtml(data) {
+  let result = `<h1>${data.customer} の支払い</h1>\n`;
+
+  result += '<table>\n';
+  result += '<tr><th>play</th><th>seats</th><th>cost</th></tr>\n';
+
+  for (let perf of data.performances) {
+    // 注文の内訳を加算
+    result += `<tr><td>${perf.play.name}</td><td>${usd(perf.amount)}</td><td>${
+      perf.audience
+    }</td></tr>\n`;
+  }
+  result += '</table>\n';
+
+  result += `<p>支払額は${usd(data.totalAmount)}</p>\n`;
+  result += `<p>次回使える特典は${data.totalVolumeCredits}ポイント</p>\n`;
   return result;
 }
