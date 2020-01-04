6981018bf41992378c6d597d33ac2968919feee6
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-04 10:50:55 +0900

    [refact] performancesにデータ付与のenrichPerformance関数を追加
    - 今はオブジェクトをコピーするだけ

--- chapter1/main.js
+++ chapter1/main.js
@@ -4,8 +4,13 @@ const invoices = require('./invoices.json');
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = { ...aPerformance };
+    return result;
+  }
 }
 function renderPlainText(data, plays) {
   let result = `${data.customer} の支払い\n`;
