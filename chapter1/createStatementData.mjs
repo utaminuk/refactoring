1e034e419a4f4b1b5af88e7749a3548559a06774
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-05 22:13:42 +0900

    [refact] createPerformanceCalculatorの作成
    - 適切にPerformanceCalculatorを選択させるためのFactory関数として機能させる

--- chapter1/createStatementData.mjs
+++ chapter1/createStatementData.mjs
@@ -44,6 +44,10 @@ class PerformanceCalculator {
   }
 }
 
+function createPerformanceCalculator(aPerformance, aPlay) {
+  return new PerformanceCalculator(aPerformance, aPlay);
+}
+
 export default function createStatementData(invoice, plays) {
   const result = {};
   result.customer = invoice.customer;
@@ -54,7 +58,7 @@ export default function createStatementData(invoice, plays) {
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(
+    const calculator = createPerformanceCalculator(
       aPerformance,
       playFor(aPerformance)
     );
