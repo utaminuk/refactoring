f8b8e5d10c68b17bab6fc4dc2b441e4c4cd3f75c
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-13 21:52:04 +0900

    [test] producersが設定されてない場合のパターンを追加

--- chapter4/test/Province.test.js
+++ chapter4/test/Province.test.js
@@ -11,4 +11,28 @@ describe('Province', () => {
   test('profitで利益計算をする', () => {
     expect(asia.profit).toBe(230);
   });
+  test('productionの変更', () => {
+    asia.producers[0].production = 20;
+    expect(asia.shortfall).toBe(-6);
+    expect(asia.profit).toBe(292);
+  });
+});
+
+describe('Producerが設定されていないパターン', () => {
+  let noProducers;
+  beforeEach(() => {
+    const data = {
+      name: 'No producers',
+      producers: [],
+      demand: 30,
+      price: 20
+    };
+    noProducers = new Province(data);
+  });
+  test('shortallで不足分計算をする', () => {
+    expect(noProducers.shortfall).toBe(30);
+  });
+  test('profitで利益計算をする', () => {
+    expect(noProducers.profit).toBe(0);
+  });
 });
