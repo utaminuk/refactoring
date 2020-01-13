f5fa287bcc2995bcd3673b083cad948905cc2eeb
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-13 21:33:35 +0900

    [test] Provinceの作成をbeforeEachに移動した

--- chapter4/test/Province.test.js
+++ chapter4/test/Province.test.js
@@ -1,12 +1,14 @@
 import { Province, sampleProvinceData } from '../src/index';
 
 describe('Province', () => {
+  let asia;
+  beforeEach(() => {
+    asia = new Province(sampleProvinceData());
+  });
   test('shortallで不足分計算をする', () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.shortfall).toBe(5);
   });
   test('profitで利益計算をする', () => {
-    const asia = new Province(sampleProvinceData());
     expect(asia.profit).toBe(230);
   });
 });
