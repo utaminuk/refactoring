ad47853662d96e53ed24638b56b741b20bb5bbf5
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-21 21:27:07 +0900

    [doc] 過去コードと比較するためにコメントアウトしてコピー

--- chapter6/src/ExtractVariable.js
+++ chapter6/src/ExtractVariable.js
@@ -4,3 +4,10 @@ export function ExtractVariable(order) {
     Math.min(order.quantity * order.itemPrice * 0.1, 100)
   );
 }
+
+// export function ExtractVariable(order) {
+//   return (
+//     order.quantity * order.itemPrice * 0.05 +
+//     Math.min(order.quantity * order.itemPrice * 0.1, 100)
+//   );
+// }
