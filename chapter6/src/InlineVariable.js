28628175e94b4af5d8153360230d02a2828bd230
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-22 21:54:41 +0900

    [refact] basePriceの変数はインライン化したほうがわかりやすいのでインライン化

--- chapter6/src/InlineVariable.js
+++ chapter6/src/InlineVariable.js
@@ -1,6 +1,5 @@
 export function isHigherThousand(anOrder) {
-  let basePrice = anOrder.basePrice;
-  return basePrice > 1000;
+  return anOrder.basePrice > 1000;
 }
 
 // export function isHigherThousand(anOrder) {
