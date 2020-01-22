cf9daba73fda6202f01f944921b587b1f0200cf5
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-22 21:53:39 +0900

    [refact] 過去のコードと比較するためにコメントアウトして一部保存しておく

--- chapter6/src/InlineVariable.js
+++ chapter6/src/InlineVariable.js
@@ -2,3 +2,8 @@ export function isHigherThousand(anOrder) {
   let basePrice = anOrder.basePrice;
   return basePrice > 1000;
 }
+
+// export function isHigherThousand(anOrder) {
+//   let basePrice = anOrder.basePrice;
+//   return basePrice > 1000;
+// }
