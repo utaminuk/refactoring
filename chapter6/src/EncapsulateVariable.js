2c1cab8efb8d655b562f0098913c40f660fdce0c
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-25 14:26:24 +0900

    [doc] 過去の状態を保存しておくためにコメントアウトしてとっておく

--- chapter6/src/EncapsulateVariable.js
+++ chapter6/src/EncapsulateVariable.js
@@ -3,3 +3,5 @@ let defaultOwnerData = { firstName: 'Martin', lastName: 'Fowler' };
 
 export function defaultOwner() { return defaultOwnerData; }
 export default defaultOwner;
+
+// let defaultOwner = { firstName: 'Martin', lastName: 'Fowler' };
