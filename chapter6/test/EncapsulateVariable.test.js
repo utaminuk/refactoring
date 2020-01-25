86ef2279cba0dae9d3033a747f55550fc4389520
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-01-25 10:23:45 +0900

    [test] EncapsulateVariableのテストを追加
    - デフォルト値をチェック

---
+++ chapter6/test/EncapsulateVariable.test.js
@@ -0,0 +1,13 @@
+import defaultOwner from '../src/EncapsulateVariable';
+
+describe('EncapsulateVariable: 変数のカプセル化', () => {
+  beforeEach(() => { });
+  afterEach(() => { });
+  test('デフォルトオーナーの配列をチェックする', () => {
+    const expected = { firstName: 'Martin', lastName: 'Fowler' }
+      ;
+    expect(defaultOwner).toStrictEqual(expected);
+  });
+
+
+});
