e9fe0d75da97747e5d0ebc31992436bbf872b5a7
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-02 11:28:55 +0900

    [feat] 最初のコードを追加

--- chapter6/src/CombineFunctionsIntoClass.js
+++ chapter6/src/CombineFunctionsIntoClass.js
@@ -1,7 +1,6 @@
-const reading = { customer: "ivan", quantity: 10, month: 6, year: 2017 };
-
+export const acquireReading = () => ({ customer: "ivan", quantity: 10, month: 6, year: 2017 });
 const round = num => Math.round(num * 100) / 100;
 export const baseRate = (year, month) => 1 + round((((year - month) / 2000) - 1) * 10);
 
-// const aReading = acquireReading();
-// const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const aReading = acquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
