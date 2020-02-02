2d5fc5cdd542ce835d28a17aad73719cab934fc9
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-02 11:37:09 +0900

    [refact] 同様処理を抽出したいので基本支払い計算を関数化
    - calculateBaseChargeを追加

--- chapter6/src/CombineFunctionsIntoClass.js
+++ chapter6/src/CombineFunctionsIntoClass.js
@@ -1,6 +1,10 @@
 export const acquireReading = () => ({ customer: "ivan", quantity: 10, month: 6, year: 2017 });
 const round = num => Math.round(num * 100) / 100;
-export const baseRate = (year, month) => 1 + round((((year - month) / 2000) - 1) * 10);
+export const baseRate = (month, year) => 1 + round((((year - month) / 2000) - 1) * 10);
 
 const aReading = acquireReading();
-const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const baseCharge = calculateBaseCharge(aReading);
+
+export function calculateBaseCharge(aReading) {
+  return round(baseRate(aReading.month, aReading.year) * aReading.quantity);
+}
--- chapter6/test/CombineFunctionsIntoClass.test.js
+++ chapter6/test/CombineFunctionsIntoClass.test.js
@@ -1,10 +1,20 @@
-import { baseRate } from '../src/CombineFunctionsIntoClass.js';
+import { baseRate, calculateBaseCharge } from '../src/CombineFunctionsIntoClass.js';
 
 describe('CombineFunctionsIntoClass.baseRate: 関数群のクラスへの集約 - 基本レート', () => {
   beforeEach(() => { });
   afterEach(() => { });
   test('baseRate: 正常系 2019, 10を入力', () => {
-    expect(baseRate(2019, 10)).toBe(1.04);
+    expect(baseRate(10, 2019)).toBe(1.04);
+  });
+
+});
+
+describe('CombineFunctionsIntoClass.calculateBaseCharge: 関数群のクラスへの集約 - 基本支払い計算', () => {
+  beforeEach(() => { });
+  afterEach(() => { });
+  test('baseRate: 正常系 2019, 10を入力', () => {
+    let expected = { customer: "ivan", quantity: 10, month: 6, year: 2017 };
+    expect(calculateBaseCharge(expected)).toBe(10.6);
   });
 
 });
