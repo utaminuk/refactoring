c4becfef01eea3978381da1ae2cfb2728e650c04
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-06 09:04:33 +0900

    [refact] 過去のコードをコメントとして残しておく

--- chapter6/src/CombineFunctionsIntoClass.js
+++ chapter6/src/CombineFunctionsIntoClass.js
@@ -2,9 +2,18 @@ export const acquireReading = () => ({ customer: "ivan", quantity: 10, month: 6,
 const round = num => Math.round(num * 100) / 100;
 export const baseRate = (month, year) => 1 + round((((year - month) / 2000) - 1) * 10);
 
+// 実コード
 const aReading = acquireReading();
 const baseCharge = calculateBaseCharge(aReading);
 
 export function calculateBaseCharge(aReading) {
   return round(baseRate(aReading.month, aReading.year) * aReading.quantity);
 }
+
+// 過去のコード
+// const aReading = acquireReading();
+// const baseCharge = calculateBaseCharge(aReading);
+
+// export function calculateBaseCharge(aReading) {
+//   return round(baseRate(aReading.month, aReading.year) * aReading.quantity);
+// }
