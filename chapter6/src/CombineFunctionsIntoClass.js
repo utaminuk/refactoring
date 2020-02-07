aae5c2b84c8297ab653f92cdd2028527560d38ce
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-07 12:52:24 +0900

    [refact] データ保存のためのReadingクラスを追加

--- chapter6/src/CombineFunctionsIntoClass.js
+++ chapter6/src/CombineFunctionsIntoClass.js
@@ -3,6 +3,20 @@ const round = num => Math.round(num * 100) / 100;
 export const baseRate = (month, year) => 1 + round((((year - month) / 2000) - 1) * 10);
 
 // 実コード
+class Reading {
+  constructor(data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+  get customer() { return this._customer; }
+  get quantity() { return this._quantity; }
+  get month() { return this._month; }
+  get year() { return this._year; }
+}
+
+
 const aReading = acquireReading();
 const baseCharge = calculateBaseCharge(aReading);
 
