a6154a5414da2bdcc1fea2c461b151eb5fe445e1
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-12 20:24:08 +0900

    [feat] baseRate関数を追加

--- chapter6/src/CombineFunctionsIntoTransform.js
+++ chapter6/src/CombineFunctionsIntoTransform.js
@@ -1,2 +1,11 @@
 export const rawReading = { customer: "ivan", quantity: 10, month: 5, year: 2017 };
 
+function baseRate(month, year) {
+  return 1 + this.round((((year - month) / 2000) - 1) * 10);
+}
+
+
+const aReading = aquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
