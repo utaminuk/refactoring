c2319b5dc73a8a7c3da50488570862ce1ddfdc47
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-13 20:44:23 +0900

    [feat] 課税対象額計算コードを追加

--- chapter6/src/CombineFunctionsIntoTransform.js
+++ chapter6/src/CombineFunctionsIntoTransform.js
@@ -6,6 +6,9 @@ function baseRate(month, year) {
 
 
 const aReading = aquireReading();
+// 基本料金
 const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
 
+// 課税対象額
 const base = (baseRate(aReading.month, aReading.year) * aReading.quantity);
+const taxableCharge = Math.max(0, base - taxThreshhold(aReading.year))
