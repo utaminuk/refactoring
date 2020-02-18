626be64cf4bf6e24f984bc65eb3bec326b4af404
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-18 15:09:16 +0900

    [refact] 価格を決定するデータをpriceDataとしてapplyShippingに追加
    - 空のオブジェクトにしているが今後引数と交換していく

--- chapter6/src/SplitPhase.js
+++ chapter6/src/SplitPhase.js
@@ -4,11 +4,12 @@ export function priceOrder(product, quantity, shippingMethod) {
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const price = applyShipping(basePrice, shippingMethod, quantity, discount);
+  const priceData = {};
+  const price = applyShipping(priceData, basePrice, shippingMethod, quantity, discount);
   return price;
 }
 
-function applyShipping(basePrice, shippingMethod, quantity, discount) {
+function applyShipping(priceData, basePrice, shippingMethod, quantity, discount) {
   const shippingPerCase =
     basePrice > shippingMethod.discountThreshold
       ? shippingMethod.discountedFee
