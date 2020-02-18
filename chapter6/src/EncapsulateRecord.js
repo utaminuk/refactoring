4318ad38541dd5702a323f10c7f70951949ec92f
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-18 17:58:00 +0900

    [doc] 実際の利用例を追加

--- chapter6/src/EncapsulateRecord.js
+++ chapter6/src/EncapsulateRecord.js
@@ -4,5 +4,9 @@ const organization = { name: "Acme Gooseberries", country: "GB" }
 
 function getRawDataOfOrganization() { return organization; }
 
+// 実際の利用例
+console.log(getRawDataOfOrganization());
+
 // 元コードの保存
 // const organization = { name: "Acme Gooseberries", country: "GB" }
+// console.log(organization);
