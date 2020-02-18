4a666de844eba10aa5fa61c5e1f1af0807eaded7
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-18 17:57:08 +0900

    [refact] データをカプセル化するための中間関数を用意

--- chapter6/src/EncapsulateRecord.js
+++ chapter6/src/EncapsulateRecord.js
@@ -1,5 +1,8 @@
 // レコードのカプセル化(p168)
 const organization = { name: "Acme Gooseberries", country: "GB" }
 
+
+function getRawDataOfOrganization() { return organization; }
+
 // 元コードの保存
 // const organization = { name: "Acme Gooseberries", country: "GB" }
