065b2246aaa5410b5db7af505c63a9524840b4c6
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-18 20:02:58 +0900

    [refact] getRawDataOfOrganizationの削除

--- chapter6/src/EncapsulateRecord.js
+++ chapter6/src/EncapsulateRecord.js
@@ -14,8 +14,6 @@ class Organization {
 
 const organization = new Organization({ name: "Acme Gooseberries", country: "GB" });
 
-function getRawDataOfOrganization() { return organization._data; }
-
 export function getOrganization() {
   return organization;
 }
