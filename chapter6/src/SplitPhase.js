fd2162f8d7b95b9e909dfa486f99982add5305f5
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-18 15:24:14 +0900

    [refact] 価格設定データを計算する関数を追加(calculatePricingData)

--- chapter6/src/SplitPhase.js
+++ chapter6/src/SplitPhase.js
@@ -1,12 +1,16 @@
 export function priceOrder(product, quantity, shippingMethod) {
+  const priceData = calculatePricingData(product, quantity);
+  const price = applyShipping(priceData, shippingMethod);
+  return price;
+}
+
+function calculatePricingData(product, quantity) {
   const basePrice = product.basePrice * quantity;
   const discount =
     Math.max(quantity - product.discountThreshold, 0) *
     product.basePrice *
     product.discountRate;
-  const priceData = { basePrice, quantity, discount };
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
+  return { basePrice, quantity, discount };
 }
 
 function applyShipping(priceData, shippingMethod) {
