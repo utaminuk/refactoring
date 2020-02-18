77db1203f382899c05777d0c3b251caf26b24bfa
ku2ma2 <motorohi.tsumaniku@gmail.com>
2020-02-18 15:25:04 +0900

    [refact] 無意味なconstの削除

--- chapter6/src/SplitPhase.js
+++ chapter6/src/SplitPhase.js
@@ -1,7 +1,6 @@
 export function priceOrder(product, quantity, shippingMethod) {
   const priceData = calculatePricingData(product, quantity);
-  const price = applyShipping(priceData, shippingMethod);
-  return price;
+  return applyShipping(priceData, shippingMethod);
 }
 
 function calculatePricingData(product, quantity) {
@@ -20,7 +19,5 @@ function applyShipping(priceData, shippingMethod) {
       : shippingMethod.feePerCase;
 
   const shippingCost = priceData.quantity * shippingPerCase;
-  const price = priceData.basePrice - priceData.discount + shippingCost;
-
-  return price;
+  return priceData.basePrice - priceData.discount + shippingCost;
 }
